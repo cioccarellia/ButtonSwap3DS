266946e7b7456f55b1dc0d73c89362cb4dc28944
mikahjc <mikah.chapman@gmail.com>
2016-09-27 18:01:51 -0400

    Code cleanup and optimization

--- injector/source/injected.s
+++ injector/source/injected.s
@@ -11,87 +11,81 @@ LDR     R0, [R0]
 SVC     0x32
 ANDS    R1, R0, #0x80000000
 BMI     .ret
-push {r3}
-ldr r3, =0x10df08
+PUSH    {r3}
+LDR     R3, =0x10df08
 LDRD    R0, [R4,#8]
 STRD    R0, [R3, #8]
-LDRD	r0, [R3]
-STRD	r0,	[R5]
+LDRD	  R0, [R3]
+STRD	  R0,	[R5]
 LDR     R0, [R4,#4]
-pop {r3}
+POP     {R3}
 
 @buttons init
-ldr r0, =0x10df20
-ldr r1, =0xFFF
-bl .init
+LDR     R0, =0x10df20
+LDR     R1, =0xFFF
+BL      .init
 
 @touch init
-ldr r0, =0x10df24
-ldr r1, =0x2000000
-bl .init
+LDR     R0, =0x10df24
+LDR     R1, =0x2000000
+BL      .init
 
 @cpad init
-ldr r0, =0x10df28
-ldr r1, =0x800800
-bl .init
+LDR     R0, =0x10df28
+LDR     R1, =0x800800
+BL      .init
 
 @buttons copy
-ldr r0, =0x10df20
-ldr r1, =0xFFF
-ldr r2, =0x1ec46000
-ldr r3, =0x10df00
-bl .swap
+LDR     R0, =0x1ec46000
+LDR     R1, =0x10df00
+BL      .swap
 
 @touch copy
-ldr r0, =0x10df24
-ldr r1, =0x2000000
-ldr r2, =0x10df10
-ldr r3, =0x10df08
-bl .copy
+LDR     R0, =0x10df24
+LDR     R1, =0x2000000
+LDR     R2, =0x10df10
+LDR     R3, =0x10df08
+BL      .copy
 
 @cpad copy
-ldr r0, =0x10df28
-ldr r1, =0x800800
-ldr r2, =0x10df14
-ldr r3, =0x10df0c
-bl .copy
+LDR     R0, =0x10df28
+LDR     R1, =0x800800
+LDR     R2, =0x10df14
+LDR     R3, =0x10df0c
+BL      .copy
 
 .ret:
 LDMFD   SP!, {R4-R6,PC}
 
 .init:
-ldr r2, [r0]
-cmp r2, #0
-streq r1, [r0]
-mov pc, r14
+LDR     R2, [R0]
+CMP     R2, #0
+STREQ   R1, [R0]
+MOV     PC, R14
 
 .copy:
-ldr r4, [r2]
-str r4, [r3]
-mov pc, r14
+LDR     R4, [R2]
+STR     R4, [R3]
+MOV     PC, R14
 
 .swap:
-LDR     R4, [R2]         // Load HID State
+LDR     R2, [R0]         // Load HID State
 
-// ARM ASM doesn't allow us to directly load a big enough
-// bitmask.  These load them into registers manually.
+MOV     R3, #0x20
+LSL     R3, R3, #4
+ADD     R3, R3, #2       // R3 Button mask
 
-MOV     R5, #0x30
-LSL     R6, R5, #4
-ADD     R5, R6, #2       // R5 XOR mask
+MOV     R4, #0x30
+LSL     R4, R4, #4
+ADD     R4, R4, #2       // R3 XOR mask
 
-MOV     R6, #0x20
-LSL     R7, R6, #4
-ADD     R6, R7, #2       // R6 Button mask
-
-AND     R0, R4, R6       // Extract desired values
-CMP     R0, #1           // See if either are pressed
-EORMI   R1, R4, R5       // If so, xor HID Register
-MOVMI   R4, R1
-STR     R4, [R3]         // Store R4 at *R3
+AND     R0, R2, R3       // Extract desired values
+CMP     R0, #0           // See if either are pressed
+EOREQ   R2, R2, R4       // If so, xor HID Register
+STR     R2, [R1]         // Store R4 at *R3
 MOV     PC, R14          // Return
 
 .LTORG # assembles literal pool
 
 read_input_sz:
-.4byte .-read_input
+.4byte  .-read_input
